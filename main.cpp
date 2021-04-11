1f974f10064c010aefd5bc1d22c7c1e612e31182
James <poyuan1004@gmail.com>
2021-04-11 17:57:01 -0400

    Add single threaded read

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
-int main() {
-    std::cout << "First Commit"
-              << "\n";
+#include <vector>
+
+struct Player {
+    int id;
+    int rank;
+};
+
+int main(int argc, char* argv[]) {
+    FILE* fin = nullptr;
+    fin       = fopen("in.txt", "rt");
+    int num   = 0;
+
+    fscanf(fin, "%d", &num);
+
+    std::vector<Player> data(num);
+
+    for (int i = 0; i < num; i++) {
+        fscanf(fin, "%d %d", &data[i].id, &data[i].rank);
+    }
+
+    fclose(fin);
+
+    printf("%d\n", num);
+    for (int i = 0; i < num; i++) {
+        printf("%d %d\n", data[i].id, data[i].rank);
+    }
     return 0;
 }
