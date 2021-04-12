774e00ec8315dafc7b5ad7cd885faeca6bb3a6e2
James <poyuan1004@gmail.com>
2021-04-11 21:51:18 -0400

    Add multi-thread to readfile

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <cstdio>
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
+
+// [todo] need to test different num threads, 
+#define NUM_THREADS 2 
+
+std::mutex mut_;
 
 struct Player {
     int id;
@@ -11,14 +17,24 @@ struct Player {
 struct ThreadData {
     FILE*                fin;
     std::vector<Player>& data;
+    int                  startIdx;
+    int                  endIdx;
 };
 
 void th_readfile(ThreadData threadata) {
-    FILE*               fin  = threadata.fin;
-    std::vector<Player> data = threadata.data;
-    int                 num  = data.size();
-    for (int i = 0; i < num; i++) {
-        fscanf(fin, "%d %d", &data[i].id, &data[i].rank);
+    FILE*                fin  = threadata.fin;
+    std::vector<Player>& data = threadata.data;
+    int                  num  = data.size();
+    for (int i = threadata.startIdx; i <= threadata.endIdx; i++) {
+        int id   = -1;
+        int rank = -1;
+
+        mut_.lock();
+        fscanf(fin, "%d %d", &id, &rank);
+        mut_.unlock();
+
+        data[i].id   = id;
+        data[i].rank = rank;
     }
 }
 
@@ -31,10 +47,27 @@ int main(int argc, char* argv[]) {
 
     std::vector<Player> data(num);
 
-    ThreadData threadata = {fin, data};
+    std::vector<std::thread> threads;
+    /*
+     0 32   33*0   33*1  0   33*i  33*(i+1)
+    33 65   33*1   33*2  1
+    66 98   33*2   33*3  2
+    */
+    for (int i = 0; i < NUM_THREADS; i++) {
+        int count      = num / NUM_THREADS;
+        int startIndex = count * i;
+        int endIndex   = count * (i + 1) - 1;
+        // Assign the rest tasks to the last thread
+        if (i == NUM_THREADS - 1) {
+            endIndex = num - 1;
+        }
+        ThreadData threadata = {fin, data, startIndex, endIndex};
+        threads.push_back(std::thread(th_readfile, threadata));
+    }
 
-    std::thread first(th_readfile, threadata);
-    first.join();
+    for (int i = 0; i < NUM_THREADS; i++) {
+        threads[i].join();
+    }
 
     fclose(fin);
 
